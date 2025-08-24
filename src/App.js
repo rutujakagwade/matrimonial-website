3a2a5bd590fd676d3e5c216683bb0b8de2370a9a
Rutuja Kagwade <rutujakagwade5@example.com>
2025-08-24 16:16:43 +0530

    Initial commit with frontend and backend

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+    <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -23,3 +23,4 @@ function App() {
 }
 
 export default App;
+  
